4b1cd9ec4d25298de473121ce79180ba9cd0f4ce
Heekyung Kim <Heekyungkim.tech@gmail.com>
2019-02-24 22:00:39 -0500

    Part2 Required

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Heekyung kim on 2/24/19.
+//  Copyright © 2019 Heekyung kim. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
